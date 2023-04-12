e30c50d5733be12f1f0d2a8de72cfc74629a18fd
hanbonghun <bng4535@khu.ac.kr>
2023-04-13 01:42:00 +0900

    style: 로그인/로그아웃에 따라 상단바 내용 수정

--- src/main/java/com/shop/config/SecureConfig.java
+++ src/main/java/com/shop/config/SecureConfig.java
@@ -8,13 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecureConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
         return http.build();
     }
 
