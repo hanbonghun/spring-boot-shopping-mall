3459d6d75a537d4259fb362b175b97d3caf6a94b
hanbonghun <bng4535@khu.ac.kr>
2023-04-12 21:16:50 +0900

    style: 레이아웃 layout, header, footer 생성

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,12 +2,19 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class ShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String home(){
+		return "index";
+	}
 }
