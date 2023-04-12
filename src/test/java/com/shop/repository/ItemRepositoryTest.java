8c3e2d46e9858c528d0db9d6fe6cb2a328c5e375
hanbonghun <bng4535@khu.ac.kr>
2023-04-12 16:33:50 +0900

    test:쿼ë¦ 쿼리 메소드 테스트 코드 작성

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import static org.assertj.core.api.Assertions.*;
+
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Comparator;
+import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations="classpath:application-test.properties")
@@ -32,6 +34,63 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+
+    }
+
+
+    public void createItemList(){
+        for(int i=1;i<=10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100); item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList =
+                itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+                for(Item item : itemList){
+                    System.out.println(item.toString());
+                }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        assertThat(itemList).isNotNull(); // itemList가 null이 아닌지 체크
+        assertThat(itemList).hasSize(4); // itemList의 크기가 3인지 체크
+        assertThat(itemList).extracting(Item::getItemNm).contains("테스트 상품1", "테스트 상품2", "테스트 상품3","테스트 상품4"); // itemList에서 Item의 이름을 추출해 item1, item2, item3가 모두 포함되어 있는지 체크
+        assertThat(itemList).extracting(Item::getPrice).isSortedAccordingTo(Comparator.reverseOrder()); // itemList에서 Item의 가격을 추출해 내림차순으로 정렬되어 있는지 체크
 
     }
 
