8181e1cc22c7ba8376a5d6109b5dfa6495d2d94a
hanbonghun <bng4535@khu.ac.kr>
2023-04-13 15:46:09 +0900

    feat: 영속성 전이,지연로딩, Auditing 적용

---
+++ src/main/java/com/shop/config/AuditorAwareImpl.java
@@ -0,0 +1,20 @@
+package com.shop.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+//현재 로그인한 사용자의 정보를 등록자와 수정자로 지정
+public class AuditorAwareImpl implements AuditorAware {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication=
+                SecurityContextHolder.getContext().getAuthentication();
+        String userId = "";
+        if(authentication != null) {
+            userId = authentication.getName();
+        }
+        return Optional.of(userId);
+    }
+}
